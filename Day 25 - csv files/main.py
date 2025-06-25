04ab485e920e2b1eec312d49ea91f09b4aad9857
kacper.kulesza <esdecek@gmail.com>
2025-06-25 16:19:37 +0200

    some csv files

--- Day 25 - csv files/main.py
+++ Day 25 - csv files/main.py
@@ -1,21 +1,23 @@
-# with open("weather_data.csv", mode="r") as f:
-#     data = f.readlines()
-
-# print(data)
-
-# import csv
-
-# with open("weather_data.csv") as f:
-#     data = csv.reader(f)
-#     next(data)
-#     temperatures = []
-#     for each in data:
-#         temperatures.append(int(each[1]))
-#     print(temperatures)
-
-
 import pandas
 
-data = pandas.read_csv("weather_data.csv")
-print(data["temp"])
-print(data)
+data = pandas.read_csv("2018_Central_Park_Squirrel_Census_-_Squirrel_Data_20250625.csv")
+# temp = data["temp"].to_list()
+
+# print(data["temp"].max())
+
+# monday = data[data["day"] == "Monday"]
+
+gray = data["Primary Fur Color"] == "Gray"
+cinnamon = data["Primary Fur Color"] == "Cinnamon"
+black = data["Primary Fur Color"] == "Black" 
+
+print(f"Gray: {gray.sum()}, Cinnamon: {cinnamon.sum()}, Black: {black.sum()}")
+
+data_dict = {
+    "Fur Color": ["Gray", "Cinnamon", "Black"],
+    "Amount": [gray.sum(), cinnamon.sum(), black.sum()]
+}
+
+df = pandas.DataFrame(data_dict)
+print(df)
+df.to_csv("squirrel_count.csv")
