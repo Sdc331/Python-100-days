e777ef7f306e62cd652dc5eb3aa49456143d6c65
kacper.kulesza <esdecek@gmail.com>
2025-06-25 23:17:24 +0200

    changes

--- Day 26 - lists, dict/main.py
+++ Day 26 - lists, dict/main.py
@@ -1,11 +1,27 @@
-# list_1 = [1, 2, 3]
-# list_2 = [n+1 for n in list_1]
-# list_multiplied = [n*2 for n in range(1,5)]
+import pandas
+# import random
+# # list_1 = [1, 2, 3]
+# # list_2 = [n+1 for n in list_1]
+# # list_multiplied = [n*2 for n in range(1,5)]
 
-names = ['Angela', 'Pete', 'Jack', 'Josephine', 'Carol', 'Peter', 'Jule']
+# names = ['Angela', 'Pete', 'Jack', 'Josephine', 'Carol', 'Peter', 'Jule']
 
-short_names = [name for name in names if len(name) < 5]
-uppercased_names = [name.upper() for name in names if len(name) > 4]
+# # short_names = [name for name in names if len(name) < 5]
+# # uppercased_names = [name.upper() for name in names if len(name) > 4]
 
-print(short_names)
-print(uppercased_names)
+# # print(short_names)
+# # print(uppercased_names)
+# student_score = {name:random.randint(1, 100) for name in names}
+# passed_students = {name:"Passed" for (name, score) in student_score.items() if score > 60}
+# print(student_score)
+# print(passed_students)
+
+
+
+user_input = input("What is your word?: ")
+split_word = user_input.split()
+data = pandas.read_csv("nato_phonetic_alphabet.csv")
+translated = {letter:code for (letter,code) in data.iterrows()}
+print(translated.letter)
+# output = [data.code for letter in split_word if letter.upper() in data.items()]
+# print(output)
