dfb1ba4fcbd48ce64651dfdb7bab0c916631286d
kacper.kulesza <esdecek@gmail.com>
2025-06-25 20:40:24 +0200

    Added func - create csv with unanswered options

--- Day 25 - csv files/us-states-game/main.py
+++ Day 25 - csv files/us-states-game/main.py
@@ -9,7 +9,6 @@ my_turtle = Turtle()
 my_turtle.speed("fastest")
 my_turtle.penup()
 my_turtle.hideturtle()
-
 ### ------- Hold state information to check against user input later ------
 # states = pandas.read_csv("50_states.csv")["state"].to_list()
 
@@ -18,11 +17,14 @@ states = data["state"].to_list()
 
 score = 0
 user_guesses = []
+to_learn = []
 game_is_on = True
 while game_is_on:
     ans = my_screen.textinput(f"{score}/50 States Guessed", "Name a state:").title()
 
 ### ------------- Check whether answer is valid and not yet guessed --------
+    if ans == "Exit":
+         break
     if ans in states and ans not in user_guesses:
             state_data = data[data.state == ans]
             # x = data.loc[data["state"] == ans].x.item()
@@ -36,5 +38,11 @@ while game_is_on:
         my_turtle.goto(0, 0)
         my_turtle.write("You won!")
         game_is_on = False
+
+for each in states:
+    if each not in user_guesses:
+         to_learn.append(each)
+to_learn = pandas.DataFrame(to_learn)
+to_learn.to_csv("Homework states.csv")
 
 my_screen.exitonclick()
