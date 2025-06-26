fa38f8e9578dac9df34b70b1f1bce99c000b8033
kacper.kulesza <esdecek@gmail.com>
2025-06-26 12:17:14 +0200

    v1.0 done

--- Day 26 - lists, dict/main.py
+++ Day 26 - lists, dict/main.py
@@ -18,10 +18,26 @@ import pandas
 
 
 
-user_input = input("What is your word?: ")
+user_input = input("What is your word?: ").upper()
 split_word = user_input.split()
 data = pandas.read_csv("nato_phonetic_alphabet.csv")
-translated = {letter:code for (letter,code) in data.iterrows()}
-print(translated.letter)
-# output = [data.code for letter in split_word if letter.upper() in data.items()]
+translated = {row.letter:row.code for (index,row) in data.iterrows()}
+output = [translated[letter] for letter in user_input]
+print(output)
+
+# print(translated)
+# output = [translated.code for translated.code in translated if translated.letter == split_word]
+# print(output)
+# for (index, row) in data.iterrows():
+#     if row.letter == "Z":
+#         print(row.code)
+
+
+# for letter in split_word:
+#     for (index, row) in data.iterrows():
+#         print(row.get(letter))
+#         if row.letter == letter.upper():
+#             output.append(row.code)
+
+
 # print(output)
