b6a181872f5d2ef12b6e761e529451bbf3b6c835
kacper.kulesza <esdecek@gmail.com>
2025-07-08 12:51:21 +0200

    Functionality added, output created as file

--- Day 44 - BeautifulSoup/main.py
+++ Day 44 - BeautifulSoup/main.py
@@ -1,37 +1,15 @@
 from bs4 import BeautifulSoup
 import requests
 
-site_data = requests.get("https://news.ycombinator.com/news")
+site_data = requests.get("https://web.archive.org/web/20200518073855/https://www.empireonline.com/movies/features/best-movies-2/")
+site_data.encoding = "utf-8"
 site_output = site_data.text
 
 soup = BeautifulSoup(site_output, 'html.parser')
 
+titles_obj = soup.find_all("h3", class_="title")
+titles = [" ".join(title.getText().split()[1:]) for title in titles_obj]
 
-# titles = soup.find_all(".titleline")
-titles = soup.select(".titleline")
-scores = soup.select(".score")
-x = 0
-for title in titles:
-    print(f"{title.a.getText()}\n{title.a.get('href')}")
-    print(scores[x].getText())
-    x += 1
-
-# print(titles)
-
-
-
-
-
-
-
-
-
-
-
-
-
-# for tag in soup.find_all("a"):
-#     # print(tag.getText())
-#     print(tag.get("href"))
-
-# print(soup.select_one(selector="p a"))
+with open("top100.txt", "a") as f:
+    for each in titles:
+        f.write(f"{titles.index(each) + 1}. {each}\n")
