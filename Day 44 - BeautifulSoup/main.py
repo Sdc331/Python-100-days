e1236e871abf958241b856dc1f2b935c54dbb936
kacper.kulesza <esdecek@gmail.com>
2025-07-08 12:27:09 +0200

    Scraping basics

--- Day 44 - BeautifulSoup/main.py
+++ Day 44 - BeautifulSoup/main.py
@@ -1,11 +1,37 @@
 from bs4 import BeautifulSoup
+import requests
 
-with open("website.html") as f:
-    data = f.read()
+site_data = requests.get("https://news.ycombinator.com/news")
+site_output = site_data.text
 
-soup = BeautifulSoup(data, 'html.parser')
+soup = BeautifulSoup(site_output, 'html.parser')
+
+
+# titles = soup.find_all(".titleline")
+titles = soup.select(".titleline")
+scores = soup.select(".score")
+x = 0
+for title in titles:
+    print(f"{title.a.getText()}\n{title.a.get('href')}")
+    print(scores[x].getText())
+    x += 1
+
+# print(titles)
+
+
+
+
+
+
+
+
+
+
+
+
+
 # for tag in soup.find_all("a"):
 #     # print(tag.getText())
 #     print(tag.get("href"))
 
-print(soup.select_one(selector="p a"))
+# print(soup.select_one(selector="p a"))
